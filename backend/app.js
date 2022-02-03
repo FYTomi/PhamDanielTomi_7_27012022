3f602c7aa5d5ec0484568f5db75f542e8715577a
Pham Daniel <tomiyoshida75@gmail.com>
2022-02-03 15:28:50 +0100

    Connection BDD

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,15 @@ const cors = require('cors');
 
 const app = express();
 
+//parse le body
 app.use(express.json());
 
-// Autorise toute les requêtes CORS
+// Autorise toute les requêtes CORS entre frontend et backend
 app.use(cors());
+
+// Connection à notre base de données
+const connectionBDD = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'Groupomania'
+  });
