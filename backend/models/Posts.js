20e3c61120bb49abf6da6de957b03415e33480a2
Pham Daniel <tomiyoshida75@gmail.com>
2022-02-10 15:42:49 +0100

    model posts

---
+++ backend/models/Posts.js
@@ -0,0 +1,30 @@
+//Création de la table Posts
+module.exports = (sequelize, DataTypes) => {
+  const Posts = sequelize.define("Posts", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    postText: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  //Relation entre un post et les différents commentaires et likes qu'il peut avoir
+  //Tous ces éléments seront supprimés si le post est supprimé (onDelete: "cascade")
+  Posts.associate = (models) => {
+    Posts.hasMany(models.Comments, {
+      onDelete: "cascade",
+    });
+
+    Posts.hasMany(models.Likes, {
+      onDelete: "cascade",
+    });
+  };
+  return Posts;
+};
