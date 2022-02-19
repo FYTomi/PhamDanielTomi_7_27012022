1aa301930314c86db93f7a6ad934d80fbe0d6fa1
Pham Daniel <tomiyoshida75@gmail.com>
2022-02-19 03:08:01 +0100

    multer

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config({ path: "./config/.env" });
 const cors = require("cors");
 //require('./config/sql');
+const path = require('path');
 
 //Importation routes
 const usersRoutes = require("./routes/user_routes");
@@ -25,6 +26,9 @@ const db = require("./models");
 app.use("/api/auth", usersRoutes);
 app.use("/posts", postsRoutes);
 app.use("/comments",commentsRoutes);
+
+//Middleware qui répond aux requête envoyés vers le dossier images et s'en serve
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 //Crée les tables issue de l'importation des models si elles n'existent pas
 db.sequelize.sync().then((req) => {
